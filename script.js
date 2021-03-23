87323068a5a4aaa598e4ae552909ed10941d3e92
Eric Hay <ericdanielhay@yahoo.com>
2021-03-22 23:35:28 -0500

    Added additional question cards and updated javascript zindex.

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
 let next = document.querySelector("#next");
 let start = document.querySelector("#start");
-document.getElementById("start").style.zIndex = 7;
-document.getElementById("next").style.zIndex = 6;
-document.getElementById("c1").style.zIndex = 4;
+let timer = document.querySelector("#timer");
+document.getElementById("start").style.zIndex = 14;
+document.getElementById("next").style.zIndex = 13;
+document.getElementById("c1").style.zIndex = 12;
 
-let slideDeck = ["c3", "c4"];
+let slideDeck = ["c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12"];
 let index = 0;
 let card = '';
-
-function nextQuestion () {
-    card = slideDeck[index];
-    card = document.getElementById(card);
-    card.style.zIndex = 5;
-    index++;
-}
-
-next.addEventListener("click", function(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    
-    nextQuestion();
-  
-});
+let timerStart = false;
 
 function startQuiz () {
     document.getElementById("start").style.zIndex = 0;
     document.getElementById("c1").style.zIndex = 0;
-    document.getElementById("c2").style.zIndex = 5;
+    document.getElementById("c2").style.zIndex = 12;
 }
+
+function nextQuestion () {
+    card = slideDeck[index];
+    card = document.getElementById(card);
+    card.style.zIndex = 12;
+    index++;
+
+    if (timerStart === false) {
+        timerStart = true;
+        countDown();
+    }
+}
+function countDown() {
+    var timeLeft = 30;
+  
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timer.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timer.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        timer.textContent = '';
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+  }
 
 start.addEventListener("click", function(event) {
     event.stopPropagation();
     event.preventDefault();
     
     startQuiz();
+    
+});
+
+next.addEventListener("click", function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    
+    nextQuestion();
   
 });
